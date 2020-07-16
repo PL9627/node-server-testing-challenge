4ad11ebccbff2501e64cb74b33858e4b11f9ee99
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-07-16 16:17:44 -0700

    finished GET /hobbits and /hobbits/:id

--- __test__/hobbits.js
+++ __test__/hobbits.js
@@ -11,11 +11,29 @@ afterAll(async () => {
 });
 
 describe("hobbits integration tests", () => {
-    it("GET /", () => {
-        const res = await supertest(server).get("/")
+  it("GET /", async () => {
+    const res = await supertest(server).get("/");
 
-        expect(res.statusCode).toBe(200)
-        expect(res.headers["content-type"]).toBe("application/json; charset=utf-8")
-        expect(res.body.message).toBe("Welcome to our API")
-    }
-)})
+    expect(res.statusCode).toBe(200);
+    expect(res.headers["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.body.message).toBe("Welcome to our API");
+  });
+
+  it("GET /hobbits", async () => {
+    const res = await supertest(server).get("/hobbits");
+
+    expect(res.statusCode).toBe(200);
+    expect(res.headers["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.body).toHaveLength(4);
+    expect(res.body[0].name).toBe("sam");
+  });
+
+  it("GET /hobbits/:id", async () => {
+    const res = await supertest(server).get("/hobbits/2");
+
+    expect(statusCode).toBe(200);
+    expect(res.headers["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.body.id).toBe(2);
+    expect(res.body.name).toBe("frodo");
+  });
+});

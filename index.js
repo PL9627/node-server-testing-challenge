8295e5a52e75cb7fc30759e3d6a5c9ea1a52c3e6
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-07-16 16:27:11 -0700

    code refactoring

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-//const hobbitsRouter = require("./hobbits/hobbits-router");
+const hobbitsRouter = require("./hobbits/hobbits-router");
 
 const server = express();
 const port = process.env.PORT || 5000;
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 server.use(cors());
 server.use(express.json());
 
-//server.use("/hobbits", hobbitsRouter);
+server.use("/hobbits", hobbitsRouter);
 server.get("/", (req, res) => {
   res.json({
     message: "Welcome to our API",
